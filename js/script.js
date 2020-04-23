cd25b9c173520d924ce38d5a1df1deb54495d0d0
Sean Goodrich <Sjgoodrich614@gmail.com>
2020-04-22 21:49:36 -0700

    Updated Script

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ $('.js-wp-header').waypoint(function(direction) {
     
 });
 
+$('.js-wp-proj').waypoint(function(direction) {
+    $('.js-wp-proj').addClass('animated fadeInUp');
+}, {
+    offset: '40%'
+    
+});
+
 $('.js-wp-proj1').waypoint(function(direction) {
     $('.js-wp-proj1').addClass('animated fadeInRight');
 }, {
@@ -28,15 +35,23 @@ $('.js-wp-proj3').waypoint(function(direction) {
 });
 
 $('.js-wp-about').waypoint(function(direction) {
-    $('.js-wp-about').addClass('animated fadeIn');
+    $('.js-wp-about').addClass('animated fadeInLeft');
+    
+}, {
+    offset: '70%'
+});
+
+$('.js-wp-skills').waypoint(function(direction) {
+    $('.js-wp-skills').addClass('animated fadeInRight');
     
 }, {
     offset: '70%'
 });
 
 $('.js-wp-contact').waypoint(function(direction) {
-    $('.js-wp-contact').addClass('animated fadeInLeft');
+    $('.js-wp-contact').addClass('animated fadeInUp');
     
 }, {
     offset: '70%'
 });
+
